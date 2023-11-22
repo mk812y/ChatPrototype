ce9da02b9f728e276dabef6fe97b79b237a27501
mk812y <mk812y@gmail.com>
2023-11-22 10:16:22 +0100

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Михаил Куприянов on 22.11.23..
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
